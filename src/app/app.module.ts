8eac14afc05bcba09c9a2ca4c7a41576ae67564e
rishav <rishav.tandon@itera.io>
2021-03-29 22:41:08 +0200

    feat(auth module): module added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { ShoppingListService } from './services/shopping-list.service';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { RecipeService } from './services/recipe.service';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { AuthComponent } from './auth/auth.component';
-import { AuthInterceptorService } from './interceptor/auth.interceptor.service';
+import { HttpClientModule } from '@angular/common/http';
 import { RecipesModule } from './recipes/recipes.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core.module';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AuthComponent,
   ],
   imports: [
     BrowserModule,
@@ -28,8 +25,9 @@ import { SharedModule } from './shared/shared.module';
     RecipesModule,
     ShoppingListModule,
     SharedModule,
+    CoreModule,
+    AuthModule,
   ],
-  providers: [ShoppingListService, RecipeService, {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true}],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
---
+++ src/app/core.module.ts
@@ -0,0 +1,15 @@
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { AuthInterceptorService } from './interceptor/auth.interceptor.service';
+import { RecipeService } from './services/recipe.service';
+import { ShoppingListService } from './services/shopping-list.service';
+
+@NgModule({
+  providers: [
+    ShoppingListService, 
+    RecipeService,
+    {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true}
+  ]
+})
+
+export class CoreModule {}
